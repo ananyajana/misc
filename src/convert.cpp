034b104c9d38ac857bd195bcd69cd9dd89362b5d
Ananya Jana <ananya.jana@gmail.com>
2017-07-03 07:38:59 +0530

    adding binary conversion
    Signed-off-by: Ananya Jana <ananya.jana@gmail.com>

--- src/convert.cpp
+++ src/convert.cpp
@@ -7,9 +7,9 @@ Author: Ananya Jana*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-unsigned long convert(char*);	// function to convert
-static unsigned long long convert2(char*, char*, base);
+static unsigned long long convert(char*, char*);	// function to convert
 
 int main()
 {
@@ -17,14 +17,15 @@ int main()
 	printf("Enter a binary/octal/hexadecimal string:\n" );
 	scanf("%s", ptr);
 	printf("The string is: %s\n", ptr);
-	convert(ptr);
+	convert(ptr, NULL);
 }
 
-static unsigned long long convert2(char* ptr, char* endptr)
+static unsigned long long convert(char* ptr, char* endptr)
 {
+	char c;
 	int base = 0;
 	const char *s;
-	char c;
+	unsigned int num;
 	
 
 	//skip the spaces
@@ -64,54 +65,23 @@ static unsigned long long convert2(char* ptr, char* endptr)
 		base = 10;
 		s += 1;
 	}
-		
+	
 	// if base is 2, then convert directly by shifting
+	if(base == 2){
+		num = 0;
+		for(; c != '\0'; c = *s++){
+			num = num << 1;
+			if(c == '1')
+				num = num | 0x01;
+			else if(c == '0')
+				num = num | 0x00;
+			else{
+				printf("Error! only valid binary digits are 0 and 1.\n");
+				exit(1);
+			}	
+		}
+		printf("The number is: %u", num);
+	}
 	
 	// for all other bases convert by the usual method: refer strtoull
-}
-
-unsigned long convert(char* ptr)
-{
-	int len = strlen(ptr), i = 2;
-	unsigned int num;
-	
-	if('0' == ptr[0]){
-		printf("Can be a valid number!");
-		if(('b' == ptr[1]) || ('B' == ptr[1])){
-			printf("A  binary number\n");
-			if(len > 130){		// actual binary number's length can be maximum 128 characters + 0b/0B  = 130 
-				printf("Number exceeds  128 bit limit.\n");
-				exit(1);	
-			}
-			num = 0;
-			for(;i < len && ptr[i]; ++i){
-				num = num << 1;
-				if(ptr[i] == '1')
-					num = num | 0x01;
-				else if(ptr[i] == '0')
-					num = num | 0x00;
-				else{
-					printf("Error! only valid binary digits are 0 and 1.\n");
-					exit(1);
-				}	
-				printf("%d th iteration.\n", i);
-			}
-			printf("The number is: %u", num);
-		}
-		else if(('x' == ptr[1])|| ('X' == ptr[1])){
-			printf("A hexadecimal number\n");
-			if(len > 34){		// actual hexadecimal number's length can be maximum 32chars + 0b/0B  = 34 
-				printf("Number exceeds 128 bit limit.\n");
-				exit(1);	
-			}
-			
-		}	
-		else{
-			if(len > 45){		// actual octal number's length can be maximum 43 chars + 0b/0B  = 45
-				printf("Number exceeds  128 bit limit.\n");
-				exit(1);	
-			}
-			printf("An octal number\n");
-		}
-	}
 }
